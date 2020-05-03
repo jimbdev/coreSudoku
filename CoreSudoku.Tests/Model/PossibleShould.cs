904f66dfffe2ff2963b63020cda795c9bf7367d0
jimbdev <jimbdev@hotmail.com>
2020-05-02 19:20:48 -0600

    Unit tests for possible

--- CoreSudoku.Tests/Model/PossibleShould.cs
+++ CoreSudoku.Tests/Model/PossibleShould.cs
@@ -13,6 +13,31 @@ namespace CoreSudoku.tests.Model
             HashSet<int> expected = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             Assert.Equal(expected, actual.poss);
+        }
+
+        [Fact]
+        public void RemovesItem()
+        {
+            HashSet<int> expected = new HashSet<int> { 1, 2, 3, 4, 6, 7, 8, 9 };
+            Possible actual = new Possible();
+
+            actual.Remove(5);
+
+            Assert.Equal(expected, actual.poss);
+
+        }
+
+        [Fact]
+        public void RemovesSubset()
+        {
+            HashSet<int> expected = new HashSet<int> { 2, 4, 6, 8};
+            Possible actual = new Possible();
+
+            HashSet<int> toRemove = new HashSet<int> { 1, 3, 5, 7, 9};
+
+            actual.Remove(toRemove);
+
+            Assert.Equal(expected, actual.poss);
 
         }
     }
